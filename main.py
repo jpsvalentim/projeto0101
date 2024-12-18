84de0e0f44a3c0b08e98fa426e921d20e95707de
Valentim <j.pedro__@hotmail.com>
2024-12-18 19:22:48 -0300

    mais um

--- main.py
+++ main.py
@@ -1,4 +1,49 @@
 print("oi! Eu sou goku!")
 # dicicionarios
-dic = {"produto A":"tigela", "cor":"azul"}
+dic = {"produto A":"armario", "cor":"azul"}
 print(dic)
+print(dic['produto A'])
+dic1 = {1:3, 2:3, 4:{"w":"e"},"oi":["py0",1,2], "idade":45}
+print(dic1)
+print(dic1["idade"])
+#tuplas
+#elas ao imutaveis
+t = ("abacaxi","aviao","cachorro","pato0")
+print(t)
+
+conjunto = ["plutao", "java", "tutano", "tutano"]
+print(len(conjunto))
+modif = set(conjunto)
+print("aqui esta seu conjunto: ", modif)
+
+#la vem as estuturas de repetição 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

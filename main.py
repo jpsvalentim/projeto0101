1c24aa7dc460f3e4c3c0083f299a6ad085b71652
Valentim <j.pedro__@hotmail.com>
2024-12-18 19:31:09 -0300

    isso ai

--- main.py
+++ main.py
@@ -1,28 +1,46 @@
-print("oi! Eu sou goku!")
-# dicicionarios
-dic = {"produto A":"armario", "cor":"azul"}
-print(dic)
-print(dic['produto A'])
-dic1 = {1:3, 2:3, 4:{"w":"e"},"oi":["py0",1,2], "idade":45}
-print(dic1)
-print(dic1["idade"])
-#tuplas
-#elas ao imutaveis
-t = ("abacaxi","aviao","cachorro","pato0")
-print(t)
+# print("oi! Eu sou goku!")
+# # dicicionarios
+# dic = {"produto A":"armario", "cor":"azul"}
+# print(dic)
+# print(dic['produto A'])
+# dic1 = {1:3, 2:3, 4:{"w":"e"},"oi":["py0",1,2], "idade":45}
+# print(dic1)
+# print(dic1["idade"])
+# #tuplas
+# #elas ao imutaveis
+# t = ("abacaxi","aviao","cachorro","pato0")
+# print(t)
 
-conjunto = ["plutao", "java", "tutano", "tutano"]
-print(len(conjunto))
-modif = set(conjunto)
-print("aqui esta seu conjunto: ", modif)
+# conjunto = ["plutao", "java", "tutano", "tutano"]
+# print(len(conjunto))
+# modif = set(conjunto)
+# print("aqui esta seu conjunto: ", modif)
 
-#la vem as estuturas de repetição 
-
-
+# #la vem as estuturas de repetição 
 
 
+# contador = 0
+# while(contador <10):
+#     print("estou agora no numero:",contador)
+#     contador+=1
+# print("encerrou aqui!")
 
+# controle = ""
 
+# while(controle != "s"):
+#     print("a. pagar")
+#     print("b. receber")
+#     print("c. consultar")
+#     print("s. sair")
+#     controle = input("Digite a opção desejada: ")
+# print("loop encerrado!")
+# if controle == "a":
+#     while(controle != "ss"):
+#         print("escolha uma das opções de  pagamentos abaixo: ")
+#         print("a. pix")
+#         print("b. cartão")
+#         input("Digite a forma de pagamento: ")
+# print("obrigado!")
 
 
 
